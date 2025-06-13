32cff9e57eb9043d42a82fb5efcd131a2278d677
carina2023808 <londoncarina2020@gmail.com>
2025-06-13 10:41:24 +0200

    Ad my project to git

---
+++ migrations/Version20250608163439.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250608163439 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'crie la colonne couleur dans la table motos';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE motos ADD couleur VARCHAR(255) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE motos DROP couleur
+        SQL);
+    }
+}
---
+++ src/Controller/MotoController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Moto;
+use App\Form\MotoForm;
+use App\Repository\MotoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+// #[Route('/moto')]
+final class MotoController extends AbstractController
+{
+    #[Route(path: '/', name: 'app_moto_index', methods: ['GET'])]
+    public function index(MotoRepository $motoRepository): Response
+    {
+        return $this->render('moto/index.html.twig', [
+            'motos' => $motoRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/moto/creer', name: 'app_moto_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $moto = new Moto();
+        $form = $this->createForm(MotoForm::class, $moto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($moto);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_moto_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('moto/new.html.twig', [
+            'moto' => $moto,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('moto/{id}/details', name: 'app_moto_show', methods: ['GET'])]
+    public function show(Moto $moto): Response
+    {
+        return $this->render('moto/show.html.twig', [
+            'moto' => $moto,
+        ]);
+    }
+
+    #[Route('moto/{id}/edit', name: 'app_moto_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Moto $moto, EntityManagerInterface $entityManager): Response
+    
+    {
+        $form = $this->createForm(MotoForm::class, $moto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_moto_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('moto/edit.html.twig', [
+            'moto' => $moto,
+            'form' => $form,
+        ]);
+
+    }
+
+    #[Route('moto/{id}/supprimer', name: 'app_moto_delete', methods: ['POST'])]
+    public function delete(Request $request, Moto $moto, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$moto->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($moto);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_moto_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
